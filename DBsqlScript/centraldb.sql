17f2ec69b311e934d5fa76d3ae4f3a6aa8c2c1ed
prabath93 <prabath.sandaruwan.13@cse.mrt.ac.lk>
2015-08-13 07:48:23 +0530

    update sqlScript with accessPriviledge

--- DBsqlScript/centraldb.sql
+++ DBsqlScript/centraldb.sql
@@ -1,4 +1,4 @@
-﻿-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: centraldb
 -- ------------------------------------------------------
@@ -47,8 +47,10 @@ CREATE TABLE `teacher` (
   `GradeOfService` varchar(5) DEFAULT NULL,
   `DateOfPromotion` date DEFAULT NULL,
   `SectionTaught` varchar(10) DEFAULT NULL,
-  `ServiceRecord` varchar(10) DEFAULT NULL,
+  `ServiceRecord` varchar(1000) DEFAULT NULL,
   `PositionInSchool` varchar(20) DEFAULT NULL,
+  `AccessPriviledge` int(11) NOT NULL,
+  `ClubIncharge` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`NIC`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -59,7 +61,6 @@ CREATE TABLE `teacher` (
 
 LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` (`NIC`, `RNo`, `NameWithIn`, `FullName`, `Gender`, `Dob`, `CivilStatus`, `Address`, `TelNoMobile`, `TelNoRecident`, `DateOfAssignmentAsTeacher`, `DateOfAssignmentToSchool`, `EducationQual`, `ProffessionalQual`, `SubjectsAndClasses`, `SubjectsWishToTeach`, `GradesWishToTeach`, `NatureOfPresentPost`, `GradeOfService`, `DateOfPromotion`, `SectionTaught`, `ServiceRecord`, `PositionInSchool`) VALUES ('033662306v','dfdfd','prabath','sandaruwan','male','2015-08-07','Unmarried','no62/ambagahagoda/beragama/agalawatta','0719561619','','2015-08-06','2015-08-06','*dfdfdf\n*dfdf\n*dfdf','*dfdf\n*dfd\n*dfdf\n','','','','','df','2015-08-07','Primary','','teacher');
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -73,44 +74,3 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed
-
-
-DROP TABLE IF EXISTS `student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `student` (
-  `AdmissionNumber` varchar(10) NOT NULL,
-  `FullNameEnglish` varchar(100) NOT NULL,
-  `FullNameSinhala` varchar(100) DEFAULT NULL,
-  `BirthDate` date DEFAULT NULL,
-  `House` varchar(20) DEFAULT NULL,
-  `Religion` varchar(25) DEFAULT NULL,
-  `Address` varchar(200) DEFAULT NULL,
-  `TelephoneNumber` varchar(20) DEFAULT NULL,
-  `DateOfAdmission` date DEFAULT NULL,
-  `ClassOfAdmission` varchar(5) DEFAULT NULL,
-  `Gender` varchar(20) NOT NULL,
-  PRIMARY KEY (`AdmissionNumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `student`
---
-
-LOCK TABLES `student` WRITE;
-/*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('130274U','Charuni kaushalya Karunanayake','චාරුනී කෞශල්‍යා කරුණානායක','1993-07-19','ගැමුණු','Buddism','Bulath kela paara, Padikkama, Gampaha.','0712324256','2014-03-03','6C','Female'),('130281M','Karunarathne Mudiyanselage karunarathne','චානක මධුරංග කරුණාරත්න','1993-10-26','Sura','Buddhism','29/A, \"Sumadura\" Bandaranayaka mawatha, Maththumagala, Ragama.','0717899366','2014-03-03','6J','Male');
-/*!40000 ALTER TABLE `student` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-08-13  0:05:16
